f7dbdfe0f1936ed9d8443ca2deff606ac12c2763
lilibing58 <https://github.com/lilibing58>
2018-08-31 13:58:25 +0800

    lop append

---
+++ append1.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SLEN 81
+#include <sys/stat.h>
+
+int main(void)
+{
+	struct stat statfile;
+	FILE *fs=NULL,*fa=NULL;
+	char file_append[SLEN];
+	char file_source[SLEN];
+	char open_style[SLEN];
+	size_t bytes;
+        long lResult=0;
+	char *buffer=NULL;
+	int srcsize,dessize,append_size;
+	puts("Enter name of destination file:");
+	gets(file_append);
+	puts("Enter open destination file style:");
+	gets(open_style);
+	if((fa=fopen(file_append,open_style))==NULL)
+	{
+		fprintf(stderr,"Can not open :%s\n",file_append);
+		exit(1);
+	}
+
+	puts("Enter name of source file:");
+if(gets(file_source) && file_source!='\0' )
+{
+	puts("Enter open source file style:");
+	gets(open_style);
+	if(strcmp(file_source,file_append)==0)
+	{
+		fputs("Can't append itself\n",stderr);
+	}
+	else if((fs=fopen(file_source,open_style))==NULL)
+        {
+                fprintf(stderr,"Can not open file_source:%s\n",file_source);
+        }
+	else
+	{
+	
+	stat(file_source,&statfile);
+	srcsize=statfile.st_size;
+	stat(file_append,&statfile);
+	dessize=statfile.st_size;
+
+	printf("source size::%d\n",srcsize);
+	printf("destination size::%d\n",dessize);
+
+	buffer=(char*)malloc(sizeof(char)*srcsize);
+	if(buffer==NULL)
+	{fputs ("Memory error",stderr); exit (2);}
+
+        lResult=fread(buffer,sizeof(char),srcsize,fs);
+	if(lResult != srcsize)
+	{fputs ("Reading error",stderr); exit (3);}
+
+       // printf("temp content:%s\n",buffer);
+	if(dessize>srcsize)
+	{
+		puts("destination file large source file ");
+		exit(4);
+	}
+	puts("please input your append bytes.\n");
+        scanf("%d\n",&append_size);
+	printf("appendsize:%d\n",append_size);
+	printf("appendsize/srcsize:%d\n",append_size/srcsize);
+        for(int i=0;i<append_size/srcsize;i++)
+        {	fwrite(buffer,sizeof(char),srcsize,fa);
+		if(append_size%srcsize>0)
+		{
+			fwrite(buffer,sizeof(char),append_size%srcsize,fa);
+		}	
+	}
+		fclose(fs);
+		free(buffer);
+		puts("appended: ok");
+	}
+}
+	fclose(fa);
+	return 0;
+}
+
+
